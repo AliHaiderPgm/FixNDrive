2400f159e3303420651b399d220db027b8aeec3a
Ali Haider <alihaider203472@gmail.com>
2024-01-15 21:47:32 +0500

    fix(app): Removed unecessary text

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@
 const ServiceCard = ({ data }) => {
     return (
         <div className="bg-white p-4 rounded-md shadow-md hover:shadow-lg cursor-pointer transition-shadow">
-            <img src={data.image} alt={data.name} className="h-48 w-full object-cover rounded-md max-w-full" />
+            <img src={data.image} alt={data.name} className="h-28 w-full object-cover rounded-md max-w-full" />
             <p className="font-bold">{data.name}</p>
             <div>
                 <p className="text-green-500 font-semibold">Available : {data.avalible}</p>
